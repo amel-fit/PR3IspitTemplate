2f1597a7c1d50e30447773c30b86533aea500364
amel-fit <husikaamel290@gmail.com>
2025-01-29 14:24:33 +0100

    report gotov

--- FIT.WinForms/Izvjestaji/frmIzvjestaji.cs
+++ FIT.WinForms/Izvjestaji/frmIzvjestaji.cs
@@ -1,15 +1,60 @@
-﻿namespace FIT.WinForms.Izvjestaji
+﻿using FIT.Data._2202IB230046;
+using Microsoft.Reporting.WinForms;
+using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+
+namespace FIT.WinForms.Izvjestaji
 {
     public partial class frmIzvjestaji : Form
     {
-
+        public Prostorija prostorija { get; set; }
         public frmIzvjestaji()
         {
             InitializeComponent();
         }
+        public frmIzvjestaji(Prostorija p) : this()
+        {
+            prostorija = p;
+        }
 
         private void frmIzvjestaji_Load(object sender, EventArgs e)
         {
+            
+            var tblStudenti = new FIT.WinForms._2202IB230046.DataSet1.tblNastavaDataTable();
+            int rb = 1;
+            for (int i = 0; i < prostorija.Nastave.Count; i++)
+            {
+                foreach(var p in prostorija.Nastave.ElementAt(i).Prisustva)
+                {
+                    var studentiRow = tblStudenti.NewtblNastavaRow();
+                    studentiRow.rb = rb.ToString();
+                    studentiRow.Predmet = prostorija.Nastave.ElementAt(i).PredmetTXT;
+                    studentiRow.Vrijeme = prostorija.Nastave.ElementAt(i).VrijemeOdrzavanja;
+                    studentiRow.BrojIndeksa = p.Student.Indeks;
+                    studentiRow.ImePrezime = $"{p.Student.Ime} {p.Student.Prezime}";
+
+                    tblStudenti.AddtblNastavaRow(studentiRow);
+                    rb++;
+                }
+                i++;
+            }
+
+            var ds = new ReportDataSource();
+            ds.Name = "pdsNastave";
+            ds.Value = tblStudenti;
+
+            reportViewer1.LocalReport.DataSources.Add(ds);
+
+            var parameters = new ReportParameterCollection();
+
+            var pProstorijaIme = new ReportParameter("pProstorijaIme", prostorija.Naziv);
+            parameters.Add(pProstorijaIme);
+
+            int brojStudenata = rb - 1;
+            var pBrojStudenata = new ReportParameter("pBrojStudenata", brojStudenata.ToString());
+            parameters.Add(pBrojStudenata);
+
+
+            reportViewer1.LocalReport.SetParameters(parameters);
 
             reportViewer1.RefreshReport();
         }
--- FIT.WinForms/_2202IB230046/frmProstorijeIB230046.cs
+++ FIT.WinForms/_2202IB230046/frmProstorijeIB230046.cs
@@ -1,5 +1,7 @@
 ﻿using FIT.Data._2202IB230046;
 using FIT.Infrastructure;
+using FIT.WinForms.Izvjestaji;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,7 +27,7 @@ namespace FIT.WinForms._2202IB230046
         private void SetSource()
         {
             dgvProstorije.DataSource = null;
-            var lstProstorije = dbContext.Prostorije.ToList();
+            var lstProstorije = dbContext.Prostorije.Include(p => p.Nastave).ThenInclude(n => n.Prisustva).ThenInclude(p => p.Student).ToList();
             foreach (var p in lstProstorije)
             {
                 p.LogoIMG = Image.FromStream(new MemoryStream(p.Logo));
@@ -50,16 +52,24 @@ namespace FIT.WinForms._2202IB230046
 
         private void dgvProstorije_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            if(e.ColumnIndex == btnNastava.Index)
+            if (e.ColumnIndex == btnNastava.Index)
             {
                 //Nastava button
                 new frmNastavaIB230046(dgvProstorije.Rows[e.RowIndex].DataBoundItem as Prostorija).Show();
 
-            }else if(e.ColumnIndex == btnPrisustvo.Index)
+            }
+            else if (e.ColumnIndex == btnPrisustvo.Index)
             {
                 //Prisustvo Button
                 new frmPrisustvoIB230046(dgvProstorije.Rows[e.RowIndex].DataBoundItem as Prostorija).Show();
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dgvProstorije.SelectedRows.Count == 0) return;
+            Prostorija p = dgvProstorije.SelectedRows[0].DataBoundItem as Prostorija;
+            new frmIzvjestaji(p).Show();
         }
     }
 }
